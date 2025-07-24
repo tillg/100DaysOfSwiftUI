ab0cdf58694ead974d83b5b5f6d6d6b7457220de
Till Gartner <till.gartner@gmail.com>
2025-07-24 14:33:07 +0200

    Implemented logic for Unit transdformation

--- 02-MeasureConverter/MeasureConverter/MeasureConverter/ContentView.swift
+++ 02-MeasureConverter/MeasureConverter/MeasureConverter/ContentView.swift
@@ -7,12 +7,40 @@
 
 import SwiftUI
 
+struct MeasureConverter {
+    var millimeter: Double
+    var centimeter: Double {
+        get {
+            return self.millimeter / 10
+        }
+        set {
+            self.millimeter = newValue * 10
+        }
+    }
+    var meter: Double {
+        get {
+            return self.millimeter / 1000
+        }
+        set {
+            self.millimeter = newValue * 1000
+        }
+    }
+    var kilometer: Double {
+        get {
+            return self.millimeter / 1000000
+        }
+        set {
+            self.millimeter = newValue * 1000000
+        }
+    }
+}
+
 struct ContentView: View {
     @State private var fromUnit: String = "Meter"
     @State private var toUnit: String = "Kilometer"
-    @State private var value = 1.0
+    @State private var converter: MeasureConverter = MeasureConverter(millimeter: 100)
     
-    private var units: [String] = ["Meter", "Kilometer", "Centimeter", "Millimeter"]
+    private var units: [String] = ["Millimeter", "Centimeter","Meter", "Kilometer"  ]
     var body: some View {
         NavigationView {
             Form {
@@ -32,19 +60,44 @@ struct ContentView: View {
                     }
                     .pickerStyle(.segmented)
                 }
-                Section ("Value in \(fromUnit)"){
-                    TextField("Value", value: $value, format: .number)
+                Section ("Value in \(fromUnit)") {
+                    switch fromUnit {
+                    case "Millimeter":
+                        TextField("Value", value: $converter.millimeter, format: .number)
+                    case "Centimeter":
+                        TextField("Value", value: $converter.centimeter, format: .number)
+                    case "Meter":
+                        TextField("Value", value: $converter.meter, format: .number)
+                    case "Kilometer":
+                        TextField("Value", value: $converter.kilometer, format: .number)
+                    default:
+                        Text("No valid From-Unit")
+                    }
                 }
                 Section("Value in \(toUnit)") {
-                    Text(42.0, format: .number)
-
+                    switch toUnit {
+                    case "Millimeter":
+                        Text(converter.millimeter, format: .number)
+                    case "Centimeter":
+                        Text(converter.centimeter, format: .number)
+                    case "Meter":
+                        Text(converter.meter, format: .number)
+                    case "Kilometer":
+                        Text(converter.kilometer, format: .number)
+                    default:
+                        Text("No valid To-Unit")
+                    }
                 }
-                        
-                }
+                
             }
+            
         }
     }
+}
+
 
 #Preview {
     ContentView()
 }
+
+    
